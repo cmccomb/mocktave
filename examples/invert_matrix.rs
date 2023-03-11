25d3f6ea9e185809f98c1abb23064368db8e2b2b
Chris McComb <ccmcc2012@gmail.com>
2023-03-11 17:58:31 -0500

    Initial decoding for scalar values

--- examples/invert_matrix.rs
+++ examples/invert_matrix.rs
@@ -1,11 +1,13 @@
 use mocktave;
 
 fn main() {
-    mocktave::eval(
+    let x = mocktave::eval(
         "\
         z = 5.24; \
         m = z*inv(eye(5, 5));\
         m(1, 2) = 5; \
+        a = 5;\
         ",
     );
+    println!("{x:?}")
 }
--- src/lib.rs
+++ src/lib.rs
@@ -6,20 +6,75 @@
 
 use bollard::container::{Config, RemoveContainerOptions};
 use bollard::Docker;
+use std::collections::HashMap;
+use std::str::FromStr;
 
 use bollard::exec::{CreateExecOptions, StartExecResults};
 use bollard::image::CreateImageOptions;
 use futures_util::stream::StreamExt;
 use futures_util::TryStreamExt;
 
+#[derive(Debug)]
+pub struct OctaveResults {
+    scalars: HashMap<String, f64>,
+    matrices: HashMap<String, Vec<Vec<f64>>>,
+}
+
+impl OctaveResults {
+    pub fn get_scalar_named(&self, name: &str) -> Option<&f64> {
+        self.scalars.get(name)
+    }
+    pub fn get_matrix_named(&self, name: &str) -> Option<&Vec<Vec<f64>>> {
+        self.matrices.get(name)
+    }
+}
+
+impl From<String> for OctaveResults {
+    fn from(output: String) -> Self {
+        let mut results = OctaveResults {
+            scalars: Default::default(),
+            matrices: Default::default(),
+        };
+
+        let split_output = output.split("\n");
+        let mut name: String = "".to_owned();
+        let mut currently_building: String = "".to_owned();
+        let scalar: f64;
+        let matrix: Vec<Vec<f64>>;
+        for line in split_output {
+            println!("{name} {currently_building}");
+            if currently_building.len() == 0 {
+                if line.starts_with("# Created") {
+                    continue;
+                } else if line.starts_with("# name: ") {
+                    name = line.to_string().replace("# name: ", "").replace("\n", "")
+                } else if line.starts_with("# type: ") {
+                    currently_building = line.to_string().replace("# type: ", "").replace("\n", "")
+                }
+            } else {
+                if currently_building == "scalar" && !line.is_empty() {
+                    results
+                        .scalars
+                        .insert(name.clone(), f64::from_str(line).unwrap());
+                    currently_building = "".to_owned();
+                } else if currently_building == "matrix" && !line.is_empty() {
+                    currently_building = "".to_owned();
+                }
+            }
+        }
+
+        results
+    }
+}
+
 /// This is the only function ou need. It evaluates octave syntax.
 /// ```
 /// use mocktave::eval;
-/// let should_be_seven = eval("5+2");
-/// assert_eq!(should_be_seven, "ans =  7\n");
+/// let should_be_seven = eval("a = 5+2");
+/// assert_eq!(*(should_be_seven.get_scalar_named("a").unwrap()), 7_f64);
 /// ```
 #[tokio::main]
-pub async fn eval(input: &str) -> String {
+pub async fn eval(input: &str) -> OctaveResults {
     const IMAGE: &str = "mtmiller/octave:latest";
     let docker = Docker::connect_with_socket_defaults().unwrap();
 
@@ -62,7 +117,7 @@ pub async fn eval(input: &str) -> String {
                 cmd: Some(vec![
                     "octave",
                     "--eval",
-                    &(input.to_string() + "save(\"-\", \"*\");"),
+                    &(input.to_string() + ";save(\"-\", \"*\");"),
                 ]),
                 ..Default::default()
             },
@@ -95,5 +150,5 @@ pub async fn eval(input: &str) -> String {
         .await
         .expect("Await fails?");
 
-    output_text.join("")
+    OctaveResults::from(output_text.join(""))
 }

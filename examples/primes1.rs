a00c3ef2793db60f89657213fd80b555d69c5544
Chris McComb <ccmcc2012@gmail.com>
2023-05-02 14:59:16 -0400

    Better primes examples

--- examples/primes1.rs
+++ examples/primes1.rs
@@ -1,12 +1,20 @@
 fn main() {
-    // A function to compute all primes less than a given number
-    fn primes(less_than_n: usize) -> Vec<Vec<f64>> {
+    // Define a function to compute all primes less than a given number
+    fn primes(less_than_n: usize) -> Vec<i32> {
         mocktave::eval(&format!("x = primes({});", less_than_n))
-            .get_matrix("x")
-            .unwrap()
+            .get_unchecked("x")
+            .into()
     }
 
+    // Calculate all primes less than 100
     let all_primes_less_than_100 = primes(100);
 
-    println!("{:#?}", all_primes_less_than_100);
+    // Check the outcome
+    assert_eq!(
+        all_primes_less_than_100,
+        vec![
+            2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41, 43, 47, 53, 59, 61, 67, 71, 73, 79, 83,
+            89, 97
+        ]
+    );
 }
--- examples/primes2.rs
+++ examples/primes2.rs
@@ -1,11 +1,16 @@
 fn main() {
-    let primes: Box<dyn Fn([i32; 1]) -> Vec<Vec<i32>>> = mocktave::wrap("primes".into());
-    let all_primes_less_than_100: Vec<Vec<i32>> = primes([100]);
+    // Define a function to compute all primes less than a given number
+    let primes = mocktave::wrap("primes".into());
+
+    // Calculate all primes less than 100
+    let all_primes_less_than_100: Vec<i32> = primes([100]);
+
+    // Check the outcome
     assert_eq!(
         all_primes_less_than_100,
-        vec![vec![
+        vec![
             2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41, 43, 47, 53, 59, 61, 67, 71, 73, 79, 83,
             89, 97
-        ]]
+        ]
     );
 }
